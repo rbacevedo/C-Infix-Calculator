d94edacae4f3245d29a345fc3e869c48e0210832
rbaceved <rbaceved@umich.edu>
2018-11-25 19:02:58 -0500

    I added some data validation to prevent division by zero and to prevent inputting the wrong input

--- Infix.cpp
+++ Infix.cpp
@@ -2,34 +2,90 @@
 #include <string>
 #include <deque>
 #include <algorithm>
+#include <windows.h>
 
 using namespace std;
 
+string char_repeater(int n, char x)
+{
+	string character = "  ";
+	for (; n > 0; n--)
+	{
+		character += x;
+	}
+	character += "\n";
+	return character;
+}
+
+
+void welcome_message()
+{
+	cout << char_repeater(104, '*');
+	cout << "  ** Hello Welcome to the calculator of your dreams                                                     **\n";
+	cout << "  ** In this calculator you can perform addition(+), substraction(-), multiplication(*) and division(/) **\n";
+	cout << "  ** This calculator sadly does not support operation on parenthesis yet                                **\n";
+	cout << "  ** An example of an operation that you can perform is 5+6*7-8/8-29                                    **\n";
+	cout << char_repeater(104, '*');
+	cout << "\n";
+	cout << "  Input Expression = ";
+}
+
+void invalid_input_message()
+{
+	system("cls");
+	cout << char_repeater(104, '*');
+	cout << "  ** Sorry you entered an invalid input expression, please try again                                     **\n";
+	cout << char_repeater(104, '*');
+	cout << "\n";
+}
+
+void result_message(int result)
+{
+	cout << "\n";
+	cout << char_repeater(40, '*');
+	cout << "  Your result is ";
+	cout << result << "\n";
+	cout << char_repeater(40, '*');
+	cout << "\n";
+}
+
+void run_again()
+{
+	cout << "\n  Want to run the calculator again? [yes, no]: ";
+}
 
 
 int main()
 {
+	string expression;
 	deque<char> operators;
 	deque<int> nums;
 	string temp = "";
 	int curr_result = 0;
-	int result;
+	int result = 0;
 	bool again = false;
 	bool invalid_input = false;
 	string input;
 	do {
+		system("cls");
 		if (invalid_input)
 		{
-			cout << "Sorry you entered the wrong expression, please try again";
+			invalid_input_message();
 		}
+		welcome_message();
 		cin >> expression;
 		for (int i = 0; i < expression.size(); ++i)
 		{
 			if (!(expression[i] == '+' || expression[i] == '-'
-				|| expression[i] == '*' || expression[i] == '/' || (int(expression[i]) > 48 && int(expression[i]) < 57)))
+				|| expression[i] == '*' || expression[i] == '/' || (int(expression[i]) >= 48 && int(expression[i]) <= 57)))
 			{
 				cout << int(expression[i]) << endl << endl;
 				invalid_input = true;
+				break;
+			}
+			else
+			{
+				invalid_input = false;
 			}
 			if (i == expression.size() - 1)
 			{
@@ -60,6 +116,12 @@ int main()
 						}
 						else
 						{
+							//To prevent division by 0
+							if (nums.back() == 0)
+							{
+								invalid_input = true;
+								break;
+							}
 							curr_result = nums[nums.size() - 2] / nums.back();
 							operators.pop_back();
 							nums.pop_back();
@@ -121,21 +183,22 @@ int main()
 
 			}
 		}
-		nums.pop_back();
-		cout << result << endl << endl;
-		cout << "Want to run the calculaltor again?[yes, no]";
-		cin >> input;
-		transform(input.begin(), input.end(), input.begin(), ::toupper);
-		if (input == "YES")
+		result_message(result);
+		if (!invalid_input)
 		{
-			again = true;
+			nums.pop_back();
+			run_again();
+			cin >> input;
+			transform(input.begin(), input.end(), input.begin(), ::toupper);
+			if (input == "YES")
+			{
+				again = true;
+			}
+			else
+			{
+				again = false;
+			}
 		}
-		else
-		{
-			again = false;
-		}
-
-
 	} while (invalid_input || again);
 
 	cout << "Thanks for using our amazing calculator";
